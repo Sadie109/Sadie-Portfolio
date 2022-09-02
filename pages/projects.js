a8faf837381626c3f44e8b594ed1652f51c72f97
Sadie109 <101196558+Sadie109@users.noreply.github.com>
2022-09-02 14:13:09 +0100

    start of project page and positioning

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,41 @@
 import React from "react";
+import styles from "../styles/project.module.css";
 
 function projects() {
-  return <div>projects</div>;
+  return (
+    <div className={styles.ParentContainer}>
+      <div className={styles.Title}>Project ShowCase</div>
+
+      <div className={styles.WorkContainer}>
+        <div className={styles.InfoContainer}>
+          <p>filler</p>
+        </div>
+        <div>
+          <img className={styles.ImgContainer} src="/image.jpg" />
+          <a>text</a>
+          <a>text</a>
+        </div>
+      </div>
+
+      {/* <div className={styles.SecondInfoContainer}>
+        <p>filler</p>
+      </div>
+      <div className={styles.SecondImgContainer}>
+      <img/>
+        <a>text</a>
+        <a>text</a>
+      </div>
+
+      <div className={styles.InfoContainer}>
+        <p>filler</p>
+      </div>
+      <div className={styles.ImgContainer}>
+      <img/>
+        <a>text</a>
+        <a>text</a>
+      </div> */}
+    </div>
+  );
 }
 
 export default projects;
