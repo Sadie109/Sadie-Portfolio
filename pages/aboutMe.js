d4203c643b69c0a1ce5ead218bcfb22b42616b04
Sadie109 <101196558+Sadie109@users.noreply.github.com>
2022-09-14 12:51:20 +0100

    Icon import and styling

--- pages/aboutMe.js
+++ pages/aboutMe.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "../styles/aboutMe.module.css";
 
-
 function aboutMe() {
   return (
     <div className={styles.ParentContainer}>
@@ -20,19 +19,20 @@ function aboutMe() {
           <div className={styles.Skills}>
             <p>Languages i have experience with:</p>
             <div className={styles.PhotoOne}>
-              <img src="/JS.png" style={{ width: 70 }} />
-              <img src="/HTML & CSS.png" style={{ width: 100 }} />
-              <img src="/Git.png" style={{ width: 70 }} />
+              <img src="/JS.png" style={{ width: 70}} />
+              <img src="/HTML.png" style={{ width: 70 }} />
+              <img src="/CSS.png" style={{ width: 70 }} />
               <img src="/PostgreSQL.png" style={{ width: 70 }} />
             </div>
           </div>
           <p>Tech i have used:</p>
           <div className={styles.PhotoTwo}>
-            <img src="/React-icon.png" style={{ width: 70 }} />
-            <img src="/Next.png" style={{ width: 70 }} />
-            <img src="/Node.png" style={{ width: 70 }} />
-            <img src="/Jest.png" style={{ width: 70 }} />
-            <img src="/cypress-logo.png" style={{ width: 70 }} />
+            <img src="/Github.png" style={{ width: 70,  height: 70 }} />
+            <img src="/React-icon.png" style={{ width: 70,  height: 70}} />
+            <img src="/Next.png" style={{ width: 70,  height: 70 }} />
+            <img src="/Node.png" style={{ width: 70,  height: 70 }} />
+            <img src="/Jest.png" style={{ width: 70,  height: 70 }} />
+            <img src="/cypress-logo.png" style={{ width: 70,  height: 70 }} />
           </div>
         </div>
       </div>
